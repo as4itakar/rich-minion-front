105485a75f39ba2c260a0fc782c6eb55fc38b252
Obukhov Vsevolod <sevaobuxov61@gmail.com>
2023-12-05 19:10:43 +0300

    fix env

--- src/utils/imagesLink.ts
+++ src/utils/imagesLink.ts
@@ -1,4 +1,7 @@
-export enum imagesLink{
-    SERVER = 'http://localhost:5000/',
-    LOCAL = 'http://localhost:3000/images/'
+const server_url = process.env.SERVER_URL || 'http://localhost:5000/'
+const local_url = process.env.LOCAL_URL || 'http://localhost:3000/'
+
+export const imagesLink = {
+    SERVER: server_url,
+    LOCAL: local_url + 'images/'
 }
